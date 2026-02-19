2c21494238fb015cbcaf9dbdeeed19e7b4a60482
zhenghaoz <zhangzhenghao@hotmail.com>
2026-02-19 09:32:55 +0800

    Add sqlite-vec support (#1172)

--- storage/vectors/sqlite_test.go
+++ storage/vectors/sqlite_test.go
@@ -15,37 +15,21 @@
 package vectors
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
 )
 
-var (
-	weaviateUri string
-)
-
-func init() {
-	// get environment variables
-	env := func(key, defaultValue string) string {
-		if value := os.Getenv(key); value != "" {
-			return value
-		}
-		return defaultValue
-	}
-	weaviateUri = env("WEAVIATE_URI", "weaviate://127.0.0.1:8080")
-}
-
-type WeaviateTestSuite struct {
+type SQLiteTestSuite struct {
 	vectorsTestSuite
 }
 
-func (suite *WeaviateTestSuite) SetupSuite() {
+func (suite *SQLiteTestSuite) SetupSuite() {
 	var err error
-	suite.Database, err = Open(weaviateUri, "gorse_")
+	suite.Database, err = Open("sqlite://:memory:", "gorse_")
 	suite.NoError(err)
 }
 
-func TestWeaviate(t *testing.T) {
-	suite.Run(t, new(WeaviateTestSuite))
+func TestSQLite(t *testing.T) {
+	suite.Run(t, new(SQLiteTestSuite))
 }
